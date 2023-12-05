70d887e59751b2a8d7566a183bf851a1207a8fb5
zDarkOrdening <zdarkordening@gmail.com>
2023-12-04 22:05:24 -0300

    Add first code-style setup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,3 @@
-
 export default function Home() {
-  return <h1>Olá</h1>
+  return <h1>Olá</h1>;
 }
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,8 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-    var prisma: PrismaClient | undefined;
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
 }
 
 const prisma = global.prisma || new PrismaClient();
