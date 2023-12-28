f65639fc046f425d46b9f426ebe25f949bfd9b21
ydanonin <fabianorocha102003@gmail.com>
2023-12-27 23:33:41 -0300

    Solve address problem and improve customer adding new field

--- src/services/customer-service.ts
+++ src/services/customer-service.ts
@@ -4,23 +4,23 @@ import repository from "@/repository/customer"
 
 const createCustomer = async (data: Customer) => {
   return repository.createCustomer(data);
-}
+};
 
 const getAllCustomers = async (filters: CustomerRequest) => {
   return repository.getCustomers(filters);
-}
+};
 
 const getCustomerById = async (id: number) => {
   return repository.getCustomer(id);
-}
+};
 
 const updatePartialCustomer = async (id: number, data: any) =>{
-  return repository.updatePartialCustomer(id, data)
-}
+  return repository.updatePartialCustomer(id, data);
+};
 
 const deleteCustomer = async (id: number) => {
   return repository.deleteCustomer(id);
-}
+};
 
 export default {
   createCustomer,
@@ -28,4 +28,4 @@ export default {
   getAllCustomers,
   updatePartialCustomer,
   deleteCustomer
-}
+};
