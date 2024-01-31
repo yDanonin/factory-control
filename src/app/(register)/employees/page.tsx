326eea5e837b5046e406cd3d010ff349e89de6b9
zDarkOrdening <zdarkordening@gmail.com>
2024-01-31 20:01:06 -0300

    Refactor header component into an aside component, add background and dynamictable components

--- src/app/(register)/employees/page.tsx
+++ src/app/(register)/employees/page.tsx
@@ -1,9 +1,11 @@
-import Header from "@/components/Header";
+import Aside from "@/components/Aside";
+import Background from "@/components/Background";
 
 export default function Home() {
   return (
     <div className="flex">
-      <Header />
+      <Background />
+      <Aside />
       <h1>Employees</h1>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,23 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
+      transitionProperty: {
+        width: "width"
+      },
+      colors: {
+        customBlue: "#002C6D",
+        customWhite: "#F8F8F8"
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
