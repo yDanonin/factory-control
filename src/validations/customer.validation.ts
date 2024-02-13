46c41be9da5ac209727022d04dcfb38b9bb099d2
ydanonin <fabianorocha102003@gmail.com>
2024-02-13 13:59:40 -0300

    Add customer form validations and Fix prettier issues

--- src/validations/customer.validation.ts
+++ src/validations/customer.validation.ts
@@ -1,16 +1,26 @@
 import * as yup from "yup";
 
 export const Step_1_Schema = yup.object().shape({
-  // name: yup.string().required("Nome é requerido"),
-  // document: yup.string().required("Documento é requerido")
+  name: yup.string().required("Nome é requerido"),
+  document: yup.number().required("Documento é requerido"),
+  status: yup.number().required("Status é requerido"),
+  cel_number: yup.number().required("Celular é requerido"),
+  phone: yup.number().required("Telefone é requerido"),
+  email: yup.string().email("Precisa ser um email valido").required("Email é requerido")
 });
 
 export const Step_2_Schema = yup.object().shape({
-  // name: yup.string().required("Nome é requerido"),
-  // document: yup.string().required("Documento é requerido")
+  zip_code: yup.number().required("CEP é requerido"),
+  neighborhood: yup.string().required("Bairro é requerido"),
+  public_place: yup.string().required("Logradouro é requerido"),
+  city: yup.string().required("Cidade é requerida"),
+  state: yup.string().required("Estado é requerido"),
+  address_number: yup.number().required("Numero é requerido"),
+  complement: yup.string()
 });
 
 export const Step_3_Schema = yup.object().shape({
-  // name: yup.string().required("Nome é requerido"),
-  // document: yup.string().required("Documento é requerido")
+  store_name: yup.string().required("Nome da loja é requerido"),
+  credit_limit: yup.number().required("Limite de Crédito é requerido"),
+  deliver: yup.number().required("Entrega/Retirada é requerido")
 });
