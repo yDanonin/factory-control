f2fa5908711f246a74f76d8e1f24b54e248024be
ydanonin <fabianorocha102003@gmail.com>
2024-04-15 10:39:58 -0300

    Refactor backend and Add a proxy to call API

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3001/api/v1/:path*"
+      }
+    ];
+  }
+};
 module.exports = nextConfig;
