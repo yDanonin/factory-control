2a74bbd6ae63b505e55ab4c802164ec05b0aca29
yDanonin <fabianorocha102003@gmail.com>
2024-06-19 21:51:06 -0300

    Fix conflicts and Add header on the registration pages.

--- src/app/register/procedures/page.tsx
+++ src/app/register/procedures/page.tsx
@@ -21,6 +21,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
+import Header from "@/components/Header";
 
 export default function Page() {
   const router = useRouter();
@@ -31,7 +32,7 @@ export default function Page() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const resp = await axios.get("api/procedures");
+        const resp = await axios.get("/api/procedures");
         setData(resp.data.data);
       } catch (err) {
         console.error(err);
@@ -119,6 +120,7 @@ export default function Page() {
         <Aside />
       </nav>
       <main className="main-layout">
+        <Header title="Processos"/>
         <DynamicTable
           isLoadingSpinner={isLoading}
           columns={columns}
