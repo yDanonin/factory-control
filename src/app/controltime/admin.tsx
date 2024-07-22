f5c2e0b2666ac4de81120b0dab92c8e7c905b7aa
Caio Vitor <githubwithcjcnch@gmail.com>
2024-07-21 23:33:06 -0300

    add mock to admin page

--- src/app/controltime/admin.tsx
+++ src/app/controltime/admin.tsx
@@ -1,32 +1,54 @@
-import axios from "axios";
-import Link from "next/link";
+// import axios from "axios";
+// import moment from "moment";
+// import Link from "next/link";
 import { useEffect, useState } from "react";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
+// import { Employee } from "@/types/employee.types";
 import { ArrowUpRight, Users } from "lucide-react";
-import { Separator } from "@/components/ui/separator";
+// import { EmployeeWorkHour } from "@/components/ClocksEmployee/columns";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import ClockActiveEmployees from "@/components/ClockActiveEmployees/ClockActiveEmployees";
+
+// const today = moment().format("YYYY-MM-DD");
 
 export const Admin = () => {
-  const [time, setTime] = useState(new Date());
+  const [qtdFuncionario, setQtdFuncionario] = useState(0);
+  // const [employeesWorkHours, setEmployeesWorkHours] = useState([]);
 
   useEffect(() => {
-    const timerId = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
+    // const fetchData = async () => {
+    //   try {
+    //     const employeesResponse = await axios.get("/api/employees");
+    //     const employees = employeesResponse.data.data;
 
-    return () => clearInterval(timerId);
+    //     setQtdFuncionario(employees.length);
+
+    //     const onlyIdEmployees = employees.map((employee: Employee) => employee.id);
+    //     console.log(onlyIdEmployees);
+
+    //     const workHoursPromises = onlyIdEmployees.map((employeeId: { id: number }) => {
+    //       console.log("entrou");
+    //       return axios.get(`/api/employees/work-hours`, {
+    //         params: {
+    //           employee_id: employeeId,
+    //           startDate: today,
+    //           endDate: today
+    //         }
+    //       });
+    //     });
+
+    //     const workHoursResponses = await Promise.all(workHoursPromises);
+    //     console.log(workHoursResponses);
+    //     const allEmployeesWorkHours = workHoursResponses.map((response) => response.data);
+    //     setEmployeesWorkHours(allEmployeesWorkHours);
+    //   } catch (err) {
+    //     console.error(err);
+    //   }
+    // };
+
+    // fetchData();
   }, []);
-  const formatTimePart = (part: number) => {
-    return part.toString().padStart(2, "0");
-  };
 
-  const hours = formatTimePart(time.getHours());
-  const minutes = formatTimePart(time.getMinutes());
-  const seconds = formatTimePart(time.getSeconds());
-
-  async function timeClick() {
-    await axios.post("/api/employees/work-hours");
-  }
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
       <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
@@ -38,7 +60,8 @@ export const Admin = () => {
             <CardTitle className="text-sm font-medium">Total da Qtd de Funcionário</CardTitle>
           </CardContent>
           <CardFooter>
-            <p className="text-2xl font-bold">2350</p>
+            {/* <p className="text-2xl font-bold">{qtdFuncionario}</p> */}
+            <p className="text-2xl font-bold">7</p>
           </CardFooter>
         </Card>
         <Card x-chunk="dashboard-01-chunk-2">
@@ -52,7 +75,7 @@ export const Admin = () => {
             </div>
           </CardContent>
           <CardFooter>
-            <p className="text-2xl font-bold">2350</p>
+            <p className="text-2xl font-bold">7</p>
           </CardFooter>
         </Card>
       </div>
@@ -63,35 +86,9 @@ export const Admin = () => {
               <CardTitle>Funcionarios Ativos Agora</CardTitle>
               <CardDescription>Funcionários que bateram o ponto hoje</CardDescription>
             </div>
-            <Button asChild size="sm" className="ml-auto gap-1">
-              <Link href="#">
-                View All
-                <ArrowUpRight className="h-4 w-4" />
-              </Link>
-            </Button>
-          </CardHeader>
-          <CardContent>aaaaaaaaaaaaaaaaa</CardContent>
-        </Card>
-        <Card x-chunk="dashboard-01-chunk-5">
-          <CardHeader>
-            <CardTitle>Ponto Online</CardTitle>
           </CardHeader>
           <CardContent>
-            <div className="flex flex-col gap-5">
-              <div className="w-full flex justify-center gap-2">
-                <p className="text-2xl text-[#2563eb] font-bold">{hours}h</p>
-                <p className="text-2xl text-black font-bold">:</p>
-                <p className="text-2xl text-[#2563eb] font-bold">{minutes}m</p>
-                <p className="text-2xl text-black font-bold">:</p>
-                <p className="text-2xl text-[#2563eb] font-bold">{seconds}s</p>
-              </div>
-            </div>
-            <Separator className="my-4" />
-            <div className="w-full flex justify-center">
-              <Button className="w-3/4" variant="outline" onClick={() => timeClick()}>
-                Bater ponto
-              </Button>
-            </div>
+            <ClockActiveEmployees />
           </CardContent>
         </Card>
       </div>
